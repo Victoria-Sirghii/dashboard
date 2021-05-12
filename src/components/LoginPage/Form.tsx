886981507f6abb94d26b5b46a81c51fc099cfe0b
Victoria Sirghii <vika.sirghii@gmail.com>
2021-05-12 09:14:35 +0300

    login style

--- src/components/LoginPage/Form.tsx
+++ src/components/LoginPage/Form.tsx
@@ -8,14 +8,16 @@ const Form: React.FC = () => {
 
   return (
     <div className="container">
-      <h2 className="title">Sign in</h2>
-      <form className="form__group field" onSubmit={submitHandler}>
-        <input type="email" className="form__field" placeholder="E-mail" name="email" id='email'/>
-        <label htmlFor="email" className="form__label">E-mail</label>
-        <input type="password" className="form__field" placeholder="Password" name="password" id='password'/>
-        <label htmlFor="password" className="form__label">Password</label>
-        <button className="btn">Sign</button>
-      </form>
+      <div className="box flex">
+        <h2 className="title">Login Form</h2>
+        <form className="form__group field flex" onSubmit={submitHandler}>
+          <input type="email" className="form__field" placeholder="E-mail" name="email" id='email'/>
+          <input type="password" className="form__field" placeholder="Password" name="password" id='password'/>
+          <a href="" className="link center">Forgot Password?</a>
+          <button className="btn">Login</button>
+          <p className="center">Not a member? <a href="" className="link">Signup now</a></p>
+        </form>
+      </div>
     </div>
   )
 }
