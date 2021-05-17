4c9456e10d27a85d80a1d1030bf93d8072fc73f3
Victoria Sirghii <vika.sirghii@gmail.com>
2021-05-17 10:06:48 +0300

    added functional fetch useQuery hook

--- src/components/Dashboard/Content/Options/Dashboard.tsx
+++ src/components/Dashboard/Content/Options/Dashboard.tsx
@@ -1,32 +1,19 @@
-import { axios } from "api";
-import { useEffect, useState } from "react";
-import {useQueries} from "react-query"
-import {useFetchQuery} from "hooks"
+import { useFetchQuery } from "hooks";
 
 export const Dashboard: React.FC = () => {
-  const [users, setUsers] = useFetchQuery("users")
-  const [post, setPost] = useFetchQuery("post")
-  console.log(users)
-  console.log(post)
-  // const [queries, setQuieries] = useState<{ key: string; }[]>([
-  //   {key: "users"},
-  //   {key: "unkown"}
-  // ])
-  
-  // const info = useQueries(queries.map((query) => {
-  //   return {
-  //     queryKey: ['key', query.key],
-  //     queryFn: async () => {
-  //       const {data} = await axios.get(query.key);
-  //       return data.data
-  //   }
-  // }}
-  // ))
- 
+  const [users] = useFetchQuery("users");
+  const [post] = useFetchQuery("post");
+
   return (
-  <div className="dashboard-container">
-    <div className="box">
-      <p>Total users: <span></span></p>
+    <div className="dashboard-container">
+      <div className="box d-flex space-around">
+        <p className="title-info">
+          Total users: <span className="total">{users.total}</span>
+        </p>
+        <p className="title-info">
+          Total posts: <span className="total">{post.total}</span>
+        </p>
+      </div>
     </div>
-  </div>)
+  );
 };
--- src/hooks/useFetchQuery.ts
+++ src/hooks/useFetchQuery.ts
@@ -1,16 +1,11 @@
 import { axios } from "api";
-import { useState } from "react";
 import { useQuery } from "react-query";
 
-
 const useFetchQuery = (key: string) => {
-  const [dataFetch, setDataFetch] = useState()
-  const {data = [], isLoading} = useQuery(["data", key], async () => {
-    const {data} = await axios.get(key);
-    setDataFetch(data)
-    return data
-  })
-  return [dataFetch]
-}
+  const { data = [] } = useQuery(["data", key], async () => {
+    const { data } = await axios.get(key);
+    return data;
+  });
+  return [data];
+};
 export default useFetchQuery;
-
