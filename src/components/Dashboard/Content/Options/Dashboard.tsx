71123d796217341563f1b2426b782464259c1de6
Victoria Sirghii <vika.sirghii@gmail.com>
2021-05-17 12:06:28 +0300

    added components with charts

--- src/components/Dashboard/Content/Options/Dashboard.tsx
+++ src/components/Dashboard/Content/Options/Dashboard.tsx
@@ -1,12 +1,13 @@
 import { useFetchQuery } from "hooks";
+import { LineChartPost, AreaChartPost, BarChartPost } from "components";
 
 export const Dashboard: React.FC = () => {
   const [users] = useFetchQuery("users");
   const [post] = useFetchQuery("post");
 
   return (
-    <div className="dashboard-container">
-      <div className="box d-flex space-around">
+    <div className="dashboard-container grid-charts">
+      <div className="box d-flex space-around totalChart">
         <p className="title-info">
           Total users: <span className="total">{users.total}</span>
         </p>
@@ -14,6 +15,9 @@ export const Dashboard: React.FC = () => {
           Total posts: <span className="total">{post.total}</span>
         </p>
       </div>
+      <AreaChartPost post={post} />
+      <LineChartPost post={post} />
+      <BarChartPost post={post} />
     </div>
   );
 };
